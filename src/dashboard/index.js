ad0ae9b9bd6f4bfe8d0749f036e40de83d1d2672
Asad Manzoor <asad33manzoor@gmail.com>
2019-09-04 14:42:46 +0500

    Dashboard Code Updated

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,29 +1,74 @@
 import React from "react";
 import Header from '../_components/header.js';
-import DashboardList from './list.js';
 import axios from 'axios';
 import qs from'querystring';
 import ReactTable from 'react-table';
 import "react-table/react-table.css";
-import Hour_list from "../apis/hour_list";
 import _ from "lodash";
 
 const dashboard_listing_url = "http://34.248.242.178/CPDCompliance/api/Member/GetMemberCPD?Year=2019&page=1&pageSize=1000&reverse=false&sortBy=CourseName";
+const host_list_url = "http://34.248.242.178/CPDCompliance/api/Lookup/LoadCPDHost";
 const hour_list_url = "http://34.248.242.178/CPDCompliance/api/Member/MemberCPDHours?Year=2019&UserName";
 
-const bearer = 'Bearer ' + localStorage.getItem('access_token')
+const bearer = 'bearer ' + localStorage.getItem('access_token');
 
 class Dashboard extends React.Component {
 	constructor() {
 		super();
 		this.state = {
 			dashboard_listing: [],
-			hour_list: [],
+			host_list: [],
+			hours_data: [],
 		}
 	};
 
 	componentDidMount() {
-		this.setState({dashboard_listing: DashboardList})
+		// Dashboard Listing
+		fetch(dashboard_listing_url, {
+			method: 'GET',
+			withCredentials: true,
+			credentials: 'include',
+			headers: {
+				'Authorization': bearer,
+				'Content-Type': 'application/json'
+			}
+		}).then(res => res.json())
+			.then((data) => {
+				let data_items = data.Items;
+				this.setState({ dashboard_listing: data_items });
+			}).catch(console.log);
+
+		// Hosts List
+		fetch(host_list_url, {
+			method: 'GET',
+			withCredentials: true,
+			credentials: 'include',
+			headers: {
+				'Authorization': 'bearer ' + localStorage.getItem('access_token'),
+				'Content-Type': 'application/json'
+			}
+		}).then(res => res.json())
+			.then((data) => {
+				this.setState({ host_list: data });
+			})
+			.catch(console.log);
+
+
+		// Hours Data
+		fetch(hour_list_url, {
+			method: 'GET',
+			withCredentials: true,
+			credentials: 'include',
+			headers: {
+				'Authorization': 'bearer ' + localStorage.getItem('access_token'),
+				'Content-Type': 'application/json'
+			}
+		}).then(res => res.json())
+			.then((data) => {
+				this.setState({ hours_data: data });
+			})
+			.catch(console.log);
+
 	}
 
 	render () {
@@ -38,11 +83,11 @@ class Dashboard extends React.Component {
 					    <div className="col-md-4">
 					        <div className="dashboard_box">
 					            <div className="pull-right green_icon">
-					                <i className="fa fa-graduation-cap"></i>
+					                <i className="fa fa-graduation-cap"> </i>
 					            </div>
 					            8 <span className="mediumfont">Hour</span>
 					            <div className="white_progress">
-					                <div className="white_progress_inner" style={{width:'100%'}}></div>
+					                <div className="white_progress_inner" style={{width:'100%'}}> </div>
 					            </div>
 					            <div className="dashboard-box-footer" id="dashBox1">
 					                <div id="courseToDo">
@@ -55,11 +100,11 @@ class Dashboard extends React.Component {
 					    <div className="col-md-4">
 					        <div className="dashboard_box">
 					            <div className="pull-right green_icon">
-					                <i className="fa fa-clock-o"></i>
+					                <i className="fa fa-clock-o"> </i>
 					            </div>
 					            0 <span className="mediumfont">Hour 0 Mins</span>
 					            <div className="white_progress">
-					                <div className="white_progress_inner" style={{width:'100%'}}></div>
+					                <div className="white_progress_inner" style={{width:'100%'}}> </div>
 					            </div>
 					            <div className="dashboard-box-footer" id="remainingdashBox">
 					                <div id="courseToDo">
@@ -72,11 +117,11 @@ class Dashboard extends React.Component {
 					    <div className="col-md-4">
 					        <div className="dashboard_box">
 					            <div className="pull-right green_icon">
-					                <i className="fa fa-trophy"></i>
+					                <i className="fa fa-trophy"> </i>
 					            </div>
 					            55 <span className="mediumfont">Hour 7  Mins</span>
 					            <div className="white_progress">
-					                <div className="white_progress_inner" style={{width:'100%'}}></div>
+					                <div className="white_progress_inner" style={{width:'100%'}}> </div>
 					            </div>
 					            <div className="dashboard-box-footer" id="completedDashBox">
 					                <div id="courseToDo">
@@ -92,13 +137,13 @@ class Dashboard extends React.Component {
 							<i className="fa fa-filter " title="" tooltip="" data-original-title="Search"> Search</i>
 						</div>
 						<div className="shadow">
-							<div layout-gt-sm="row" className="layout-gt-sm-row">
+							<div className="layout-gt-sm-row">
 								<div style={{padding: '1rem'}}>
 
 									<div className="form-group input-group" style={{width: '100%'}}>
 										<span className="has-float-label" style={{width: '50%'}}>
-											<input className="form-control ng-pristine ng-valid ng-empty ng-touched" ng-model="vm.pagingInfo.CourseName" type="text" id="courseName" placeholder="Course Name" aria-invalid="false" />
-											<label htmlFor="courseName">Course Name</label>
+											<input className="form-control ng-pristine ng-valid ng-empty ng-touched" type="text" id="courseName" placeholder="Course Name" aria-invalid="false" />
+											<label htmlFor="course_name">Course Name</label>
 										</span>
 									</div>
 
@@ -106,20 +151,20 @@ class Dashboard extends React.Component {
 											<select className="form-control ng-pristine ng-valid ng-not-empty ng-touched" id="Year" aria-invalid="false" >
 												<option aria-checked="true" value="number:2018">2018</option>
 												<option aria-checked="true" value="number:2019" defaultValue>2019</option>
-												<option ng-repeat="year in vm.years" ng-value="year" aria-checked="true" value="number:2020">2020</option>
+												<option aria-checked="true" value="number:2020">2020</option>
 											</select>
 											<label htmlFor="Year">Year</label>
 										</div>
 										<div className="form-group input-group" style={{width: '100%'}}>
 											<span className="has-float-label">
-												<input className="form-control" type="text" placeholder="Location Name" />
-												<label htmlFor="LocationName">Location Name</label>
+												<input className="form-control" type="text" name="location_name" id="location_name" placeholder="Location Name" />
+												<label htmlFor="location_name">Location Name</label>
 											</span>
 											<div className="has-float-label" >
 												<p className="input-group">
-													<input type="text" className="form-control ng-untouched ng-empty ng-dirty ng-invalid ng-invalid-parse" name="courseDate" placeholder="Enter Start Date" ng-model="vm.pagingInfo.StartDate" my-date-picker="" end-date="+3y" aria-invalid="true" />
+													<input type="text" className="form-control ng-untouched ng-empty ng-dirty ng-invalid ng-invalid-parse" name="courseDate" placeholder="Enter Start Date" my-date-picker="" end-date="+3y" aria-invalid="true" />
 													<span className="input-group-addon" data-toggle="datepicker">
-														<span className="glyphicon glyphicon-calendar"></span>
+														<span className="glyphicon glyphicon-calendar"> </span>
 													</span>
 												</p>
 												<label htmlFor="courseDate">Enter Start Date</label>
@@ -129,17 +174,17 @@ class Dashboard extends React.Component {
 											<div className="has-float-label" >
 												<select className="form-control ng-pristine ng-valid ng-empty ng-touched">
 													<option value="" defaultValue></option>
-													{this.state.hour_list.map((item, key) =>
+													{this.state.host_list.map((item, key) =>
 														<option value={item.ID} >{item.Name}</option>
 													)}
 												</select>
 												<label htmlFor="host">Host</label>
 											</div>
 										</div>
-										<div className="clearfix"></div>
+										<div className="clearfix"> </div>
 											<div>
-												<button className="btn btn-primary"><span className="glyphicon glyphicon-search"></span> Search</button>
-												<button className="btn btn-primary"><span className="glyphicon glyphicon-remove-sign"></span> Clear</button>
+												<button className="btn btn-primary"><span className="glyphicon glyphicon-search"> </span> Search</button>
+												<button className="btn btn-primary"><span className="glyphicon glyphicon-remove-sign"> </span> Clear</button>
 											</div>
 										</div>
 									</div>
@@ -149,10 +194,10 @@ class Dashboard extends React.Component {
 						<div className="row" style={{paddingBottom: '30px'}}>
 							<div className="gridTopButtons">
 								<button type="button" className="btn btn-danger btn-circle btn-lg ">
-									<i className="fa fa-print"></i>
+									<i className="fa fa-print"> </i>
 								</button>
 								<button type="button" className="btn btn-success btn-circle btn-lg" style={{marginLeft: '10px'}}>
-									<i className="fa fa-file-excel-o"></i>
+									<i className="fa fa-file-excel-o"> </i>
 								</button>
 							</div>
 						</div>
